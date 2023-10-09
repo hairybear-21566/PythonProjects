0d3fcdb3e7a05951726e6cda60823309e3946895
f78947tg <tyler.goyea@student.manchester.ac.uk>
2023-10-09 22:43:16 +0100

    decryption successful

--- decryption2.py
+++ decryption2.py
@@ -5,7 +5,7 @@ plaintextPosition = 0
 
 while (plaintextPosition < len(plaintext)):
     plaintextChar = plaintext[plaintextPosition]
-    ASCIIValue = ord(plaintext[plaintextPosition]) - 3
+    ASCIIValue = ord(plaintext[plaintextPosition]) + 3
     if plaintext[plaintextPosition] in ['a', 'b', 'c', 'A', 'B', 'C']:
         ASCIIValue += 27
     cipherText = cipherText+chr(ASCIIValue)
